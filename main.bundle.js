b79534a12d5518887c2b0971a75d0927d912dd4d
Ben Porter <bbp5280@gmail.com>
2017-08-16 11:54:27 -0600

    background image loading, no animation, small refactor done.

--- main.bundle.js
+++ main.bundle.js
@@ -45,10 +45,40 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 	const Bird = __webpack_require__(1);
+	// const Ground = require('./ground.js');
 	let style = __webpack_require__(2);
 
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
+	const canvas = document.getElementById('canvas');
+	const context = canvas.getContext('2d');
+
+	// function drawBkg() {
+	//   let x = 0
+	//   let y = 0
+	//   let bkgImage = new Image()
+	//   bkgImage.src = '../assets/flappy_bckg.png';
+	//   context.drawImage(bkgImage, x, y)
+	//   }
+
+
+	class Background {
+	  constructor() {
+	    this.x = 0;
+	    this.y = 0;
+	    // this.width = width;
+	    // this.height = height;
+	  }
+
+	  draw(context) {
+	    let bkgImage = new Image();
+	    bkgImage.src = '../assets/flappy_bckg.png';
+	    context.drawImage(bkgImage, this.x, this.y);
+	  }
+
+	  move(velocity) {
+	    // this.y += velocity;
+	    this.x += 1;
+	  }
+	}
 
 	var bird = new Bird(20, 20);
 
@@ -56,24 +86,32 @@
 
 	function gameLoop() {
 	  context.clearRect(0, 0, canvas.width, canvas.height);
+	  background.draw(context);
 	  bird.draw(context);
 	  birdDown();
 	  requestAnimationFrame(gameLoop);
 	}
 
 	function birdUp() {
-	  context.clearRect(0, 0, canvas.width, canvas.height);
+	  // context.clearRect(0,0, canvas.width, canvas.height);
 	  bird.y -= 40;
-	  bird.draw(context);
+	  // bird.draw(context);
 	}
 
 	function birdDown() {
-	  context.clearRect(0, 0, canvas.width, canvas.height);
+	  // context.clearRect(0,0, canvas.width, canvas.height);
 	  bird.y++;
-	  bird.draw(context);
+	  // bird.draw(context);
 	}
 
 	document.addEventListener('click', birdUp);
+
+	function drawGround() {
+	  context.fillStyle = "black";
+	  context.fillRect(0, 610, 50, 30);
+	  context.fillStyle = "blue";
+	  context.fillRect(50, 610, 50, 30);
+	}
 
 	requestAnimationFrame(gameLoop);
 
