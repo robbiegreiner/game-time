a2076e62ca13c1329ff047dd6120a40296bc5831
Ben Porter <bbp5280@gmail.com>
2017-08-23 13:40:15 -0600

    ESLINT stuff

--- test/bird-test.js
+++ test/bird-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 const Bird = require('../lib/Bird.js');
 
-describe('Bird', function(){
+describe('Bird', function() {
 
   it('should be a function', () => {
     assert.isFunction(Bird);
@@ -10,43 +10,51 @@ describe('Bird', function(){
 
   it('should be an object', () => {
     const birdy = new Bird()
+
     assert.isObject(birdy);
   })
 
   it('should start with an x coordinate of 240', () => {
     const birdy = new Bird (240, 250, 60, 48)
+
     assert.equal(birdy.x, 240);
   })
 
   it('should start with a y coordinate of 250', () => {
     const birdy = new Bird (240, 250, 60, 48)
+
     assert.equal(birdy.y, 250);
   })
 
   it('should have a default width of 60', () =>{
     const birdy = new Bird (240, 250, 60, 48)
+
     assert.equal(birdy.width, 60);
   })
 
   it('should have a default height of 48', () =>{
     const birdy = new Bird (240, 250, 60, 48)
+
     assert.equal(birdy.height, 48);
   })
 
   it('should have a method called gravity', () => {
     const birdy = new Bird();
+
     assert.isFunction(birdy.gravity);
   })
 
   it('should go down 1.5 on the y axis when gravity is called once', () => {
     const birdy = new Bird (240, 250, 60, 48)
+
     birdy.gravity();
     assert.equal(birdy.y, 251.5)
   })
 
   it('should go down 2 on the y axis when gravity is called 15 times', () => {
     const birdy = new Bird (240, 250, 60, 48)
-    for (let i=0; i < 15; i++){
+
+    for (let i = 0; i < 15; i++) {
       birdy.gravity();
     }
     assert.equal(birdy.y, 274.5)
@@ -54,11 +62,13 @@ describe('Bird', function(){
 
   it('should have a method called flyUp', () => {
     const birdy = new Bird (240, 250, 60, 48)
+
     assert.isFunction(birdy.flyUp);
   })
 
   it('should go up 40 on the y axis when flyUp is called', () => {
     const birdy = new Bird (240, 250, 60, 48)
+    
     birdy.flyUp();
     assert.equal(birdy.y, 210);
   })
--- test/obstacle-test.js
+++ test/obstacle-test.js
@@ -2,44 +2,51 @@ const chai = require('chai');
 const assert = chai.assert;
 const Obstacle = require('../lib/Obstacle.js');
 
-describe('Obstacle', function(){
+describe ('Obstacle', function () {
 
-  it('should be a function', () => {
+  it ('should be a function', () => {
     assert.isFunction(Obstacle);
   });
 
   it('should be an object', () => {
     const pipe = new Obstacle()
+
     assert.isObject(pipe);
   })
 
   it('should take an argument for its starting x coordinate', () => {
     const pipe = new Obstacle(720);
+
     assert.equal(pipe.x, 720);
   })
 
   it('should take an argument for its starting y coordinate', () => {
     const pipe = new Obstacle(720, 480);
+
     assert.equal(pipe.y, 480);
   })
 
   it('should take an argument for its width', () => {
     const pipe = new Obstacle(720, 480, 100);
+
     assert.equal(pipe.width, 100);
   })
 
   it('should take an argument for its height', () => {
     const pipe = new Obstacle(720, 480, 100, 300);
+
     assert.equal(pipe.height, 300);
   })
 
   it('should have a method called obstacleMove', () => {
     const pipe = new Obstacle(720, 480, 100, 300);
+
     assert.isFunction(pipe.obstacleMove);
   })
 
   it('should move to left on the axis when a speed is passed and obstacleMove is called', () =>{
     const pipe = new Obstacle(720, 480, 100, 300);
+
     pipe.obstacleMove(1.5);
     assert.equal(pipe.x, 718.5);
 
